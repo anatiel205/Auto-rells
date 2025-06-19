e2d7ecb01270ec2967f322692e311e537fdf92fc
anatiel205 <anatielfelipe210@gmail.com>
2025-06-19 16:10:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-from flask import Flask, render_template, request
-from Crud import criar_tabela, salvar_usuario, buscar_usuario
+from flask import Flask, render_template, request, redirect
+from Crud import criar_tabela, criar_usuario, listar_usuarios
 
 app = Flask(__name__)
-criar_tabela()  # Garante que a tabela existe
 
-@app.route("/", methods=["GET"])
+# Garante que a tabela será criada ao iniciar
+criar_tabela()
+
+@app.route("/")
 def index():
-    return render_template("index.html")
+    usuarios = listar_usuarios()
+    return render_template("index.html", usuarios=usuarios)
 
-@app.route("/painel", methods=["POST"])
+@app.route("/painel")
 def painel():
-    token = request.form.get("token")
-    legenda = request.form.get("legenda")
-    tempo = int(request.form.get("tempo"))
-    termo = request.form.get("termo")
+    return render_template("painel.html")
 
-    salvar_usuario(token, legenda, tempo, termo)
-    user = buscar_usuario(token)
+@app.route("/cadastrar", methods=["POST"])
+def cadastrar():
+    nome = request.form["nome"]
+    token = request.form["token"]
+    legenda = request.form["legenda"]
+    termo = request.form["termo"]
+    intervalo = int(request.form["intervalo"])
 
-    return render_template("painel.html", usuario=user)
+    criar_usuario(nome, token, legenda, termo, intervalo)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
