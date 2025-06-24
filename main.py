dde622f357dc0ad6fd2b76d0891448f702795ba5
anatiel205 <anatielfelipe210@gmail.com>
2025-06-24 00:47:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
-from Crud import listar_usuarios, criar_usuario
+from flask import Flask, Response
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return redirect('/painel')
-
-@app.route('/painel')
-def painel():
-    usuarios = listar_usuarios()
-    return render_template("painel.html", usuarios=usuarios)
-
-@app.route('/adicionar', methods=['GET', 'POST'])
-def adicionar():
-    if request.method == 'POST':
-        nome = request.form['nome']
-        token = request.form['token']
-        legenda = request.form['legenda']
-        termo = request.form['termo']
-        intervalo = int(request.form['intervalo'])
-        criar_usuario(nome, token, legenda, termo, intervalo)
-        return redirect(url_for('painel'))
-    return render_template('adicionar.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/tiktok-verification.txt')
+def tiktok_verification():
+    return Response("tiktok-domain-verification= jisporyZZ5E4BwvQPexb6MJd7R6paF5X", mimetype='text/plain')
